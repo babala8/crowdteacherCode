8b717cd0fe4e0273200bba3d42f178e5bc7ff561
dyli <dyli@163.com>
2021-10-21 12:10:48 +0800

    单一架构SSM项目，后台管理系统，第一次提交

---
+++ atcrowdfunding01-admin-parent/src/main/java/com/atguigu/spring/boot/Pro11SpringBootMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro11SpringBootMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro11SpringBootMavenProjectApplication.class, args);
+	}
+
+}
---
+++ atcrowdfunding01-admin-parent/src/test/java/com/atguigu/spring/boot/Pro11SpringBootMavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro11SpringBootMavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
